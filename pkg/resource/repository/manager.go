67d02ba79b80f998fed811de9039704550fea076
Amine <hilalyamine@gmail.com>
2022-03-25 21:34:33 +0100

    Late initialize `RegistryID` field of Repository spec (#41)
    Description of changes:
    
    While running soak tests and local e2e tests, we observed some very
    rare runtime panics during resource reconciliation when the update
    code path is triggered. The reason was due to the controller trying
    to access a nil value of `RegistryID`.
    This specific case happened whenever the ECR DescribeRepository API call
    returned a NotFound error right after creating a repository. This chain
    of events causes the controller to not populate the `RegistryID`, hence
    the nil pointer runtime panics during the update code path.
    
    This patch late initialize the `RegistryID` field to prevent the runtime
    panic describe above.
    By submitting this pull request, I confirm that my contribution is made under
    the terms of the Apache 2.0 license.

--- pkg/resource/repository/manager.go
+++ pkg/resource/repository/manager.go
@@ -45,7 +45,7 @@ var (
 // +kubebuilder:rbac:groups=ecr.services.k8s.aws,resources=repositories,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=ecr.services.k8s.aws,resources=repositories/status,verbs=get;update;patch
 
-var lateInitializeFieldNames = []string{}
+var lateInitializeFieldNames = []string{"RegistryID"}
 
 // resourceManager is responsible for providing a consistent way to perform
 // CRUD operations in a backend AWS service API for Book custom resources.
@@ -236,6 +236,10 @@ func (rm *resourceManager) LateInitialize(
 func (rm *resourceManager) incompleteLateInitialization(
 	res acktypes.AWSResource,
 ) bool {
+	ko := rm.concreteResource(res).ko.DeepCopy()
+	if ko.Spec.RegistryID == nil {
+		return true
+	}
 	return false
 }
 
@@ -245,7 +249,12 @@ func (rm *resourceManager) lateInitializeFromReadOneOutput(
 	observed acktypes.AWSResource,
 	latest acktypes.AWSResource,
 ) acktypes.AWSResource {
-	return latest
+	observedKo := rm.concreteResource(observed).ko.DeepCopy()
+	latestKo := rm.concreteResource(latest).ko.DeepCopy()
+	if observedKo.Spec.RegistryID != nil && latestKo.Spec.RegistryID == nil {
+		latestKo.Spec.RegistryID = observedKo.Spec.RegistryID
+	}
+	return &resource{latestKo}
 }
 
 // IsSynced returns true if the resource is synced.
